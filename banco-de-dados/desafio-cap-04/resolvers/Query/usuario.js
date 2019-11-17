989a60d242913d3580a490bbdb9ee34e8c0b3970
Gabriel Jony <gabjony@gmail.com>
2019-11-17 14:18:15 -0300

    Desafio Banco de Dados & GraphQL - Query dos usuários

--- banco-de-dados/desafio-cap-04/resolvers/Query/usuario.js
+++ banco-de-dados/desafio-cap-04/resolvers/Query/usuario.js
@@ -1,10 +1,23 @@
 const db = require('../../config/db')
 
 module.exports = {
-    async usuarios() {
-        // implementar
+    //retorna todos os usuários
+    usuarios() {
+        return db('usuarios')
     },
     async usuario(_, { filtro }) {
-        // implementar
+        if(!filtro) return null
+        const { id, email } = filtro
+        if(id) {
+            return db('usuarios')
+                .where({ id })
+                .first()
+        } else if(email) {
+            return db('usuarios')
+                .where({ email })
+                .first()
+        } else {
+            return null
+        }
     },
 }
