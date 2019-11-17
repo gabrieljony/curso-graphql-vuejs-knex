e27f35ae731157c8a6e2270b0103ff6f212d8368
Gabriel Jony <gabjony@gmail.com>
2019-11-17 16:05:36 -0300

    Seção 4: Banco de Dados & GraphQL - Mutation de inserir um novo usuário

--- banco-de-dados/desafio-cap-04/resolvers/Mutation/usuario.js
+++ banco-de-dados/desafio-cap-04/resolvers/Mutation/usuario.js
@@ -1,8 +1,39 @@
 const db = require('../../config/db')
+const { perfil: obterPerfil } = require('../Query/perfil')
+const { usuario: obterUsuario } = require('../Query/usuario')
 
 module.exports = {
     async novoUsuario(_, { dados }) {
-        // Implementar
+        try {
+            const idsPerfis = []
+            if(dados.perfis) { //obter perfis
+                for(let filtro of dados.perfis) {
+                    const perfil = await obterPerfil(_, {
+                        filtro
+                    })
+                    if(perfil) idsPerfis.push(perfil.id)
+                }
+            }
+
+            // delete dados.perfis
+
+            const [ id ] = await db('usuarios')
+                .insert({
+                    nome: dados.nome,
+                    email: dados.email,
+                    senha: dados.senha
+                })
+
+            for(let perfil_id of idsPerfis) {//laço com os perfis relacionados
+                await db('usuarios_perfis')
+                    .insert({ perfil_id, usuario_id: id })
+            }
+
+            return db('usuarios')
+                .where({ id }).first()
+        } catch(e) {
+            throw new Error(e.sqlMessage)
+        }
     },
     async excluirUsuario(_, { filtro }) {
         // Implementar
