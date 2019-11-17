0feba5c5457d8e2b5cb9851cd41863f492c183a3
Gabriel Jony <gabjony@gmail.com>
2019-11-17 10:59:52 -0300

    Seção 4: Banco de Dados & GraphQL - 54. Migrations #02

--- banco-de-dados/projeto/migrations/20191117113418_tabela_perfis.js
+++ banco-de-dados/projeto/migrations/20191117113418_tabela_perfis.js
@@ -4,6 +4,12 @@ exports.up = function (knex, Promise) {
         table.increments('id').primary()
         table.string('nome').notNull().unique()
         table.string('rotulo').notNull()
+    }).then(function() {
+        return knex('perfis').insert([
+            { nome: 'comum', rotulo: 'Comum' },
+            { nome: 'admin', rotulo: 'Administrador' },
+            { nome: 'master', rotulo: 'Master' }
+        ])
     })
 };
 // Ao contrário do método up - decrementar
