60c72701f937a2e7158a347bdf0764293960b252
Gabriel Jony <gabjony@gmail.com>
2019-11-17 14:17:59 -0300

    Desafio Banco de Dados & GraphQL - Query dos perfis

--- banco-de-dados/desafio-cap-04/resolvers/Query/perfil.js
+++ banco-de-dados/desafio-cap-04/resolvers/Query/perfil.js
@@ -1,10 +1,24 @@
 const db = require('../../config/db')
 
 module.exports = {
-    async perfis() {
-        // implementar
+    //busca por todos os perfis
+    perfis() {
+        return db('perfis')
     },
+    //busca perfil passando um parametro(filtro)
     async perfil(_, { filtro }) {
-        // implementar
+        if(!filtro) return null
+        const { id, nome } = filtro
+        if(id) {
+            return db('perfis')
+                .where({ id })
+                .first()
+        } else if(nome) {
+            return db('perfis')
+                .where({ nome })
+                .first()
+        } else {
+            return null
+        }
     }
 }
