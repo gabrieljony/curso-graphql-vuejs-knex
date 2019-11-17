5cc91b45c73276303b868e57c70c7259e40896a5
Gabriel Jony <gabjony@gmail.com>
2019-11-17 14:23:22 -0300

    Desafio Banco de Dados & GraphQL - Query apartir do usuário, buscar os perfis que o usuário possui

--- banco-de-dados/desafio-cap-04/resolvers/Type/Usuario.js
+++ banco-de-dados/desafio-cap-04/resolvers/Type/Usuario.js
@@ -2,6 +2,18 @@ const db = require('../../config/db')
 
 module.exports = {
     async perfis(usuario) {
-        // implementar (Assunto novo!)
+        return db('perfis')
+            .join(
+                'usuarios_perfis',
+                'perfis.id',
+                'usuarios_perfis.perfil_id'
+            )
+            .where({ usuario_id: usuario.id })
     }
 }
+
+// #a consulta a ser realizada
+// SELECT p.*
+// FROM perfis p, usuarios_perfis up
+// WHERE up.perfil_id = p.id 
+// AND up.usuario_id = 1;
