f4994d51b001b4ad332530d3f07a31a864f74408
Gabriel Jony <gabjony@gmail.com>
2020-05-21 00:20:27 -0300

    Seção 5: Projeto: Backend com GraphQL - 76. Registrar Usuário

--- projeto-final/backend/resolvers/Mutation/usuario.js
+++ projeto-final/backend/resolvers/Mutation/usuario.js
@@ -1,83 +1,103 @@
-const db = require('../../config/db')
-const { perfil: obterPerfil } = require('../Query/perfil')
-const { usuario: obterUsuario } = require('../Query/usuario')
+const bcrypt = require("bcrypt-nodejs");
+const db = require("../../config/db");
+const { perfil: obterPerfil } = require("../Query/perfil");
+const { usuario: obterUsuario } = require("../Query/usuario");
 
-module.exports = {
-    async novoUsuario(_, { dados }) {
-        try {
-            const idsPerfis = []
-            if(dados.perfis) {
-                for(let filtro of dados.perfis) {
-                    const perfil = await obterPerfil(_, {
-                        filtro
-                    })
-                    if(perfil) idsPerfis.push(perfil.id)
-                }
-            }
+const mutations = {
+  registrarUsuario(_, { dados }) {
+      return mutations.novoUsuario(_, {
+          dados: {
+              nome: dados.nome,
+              email: dados.email,
+              senha: dados.senha,
+              perfis: null
+          }
+      })
+  },
+  async novoUsuario(_, { dados }) {
+    try {
+      const idsPerfis = [];
 
-            delete dados.perfis
-            const [ id ] = await db('usuarios')
-                .insert(dados)
+      if (!dados.pefil || !dados.perfil.lengh) {
+        dados.perfis = [
+          {
+            nome: "comum",
+          },
+        ];
+      }
 
-            for(let perfil_id of idsPerfis) {
-                await db('usuarios_perfis')
-                    .insert({ perfil_id, usuario_id: id })
-            }
+      for (let filtro of dados.perfis) {
+        const perfil = await obterPerfil(_, {
+          filtro,
+        });
+        if (perfil) idsPerfis.push(perfil.id);
+      }
 
-            return db('usuarios')
-                .where({ id }).first()
-        } catch(e) {
-            throw new Error(e.sqlMessage)
-        }
-    },
-    async excluirUsuario(_, args) {
-        try {
-            const usuario = await obterUsuario(_, args)
-            if(usuario) {
-                const { id } = usuario
-                await db('usuarios_perfis')
-                    .where({ usuario_id: id }).delete()
-                await db('usuarios')
-                    .where({ id }).delete()
-            }
-            return usuario
-        } catch(e) {
-            throw new Error(e.sqlMessage)
-        }
+      // criptografar a senha do usuario
+      const salt = bcrypt.genSaltSync();
+      dados.senha = bcrypt.hashSync(dados.senha, salt);
 
-    },
-    async alterarUsuario(_, { filtro, dados }) {
-        try {
-            const usuario = await obterUsuario(_, { filtro })
-            if(usuario) {
-                const { id } = usuario
-                if(dados.perfis) {
-                    await db('usuarios_perfis')
-                        .where({ usuario_id: id }).delete()
+      delete dados.perfis;
+      const [id] = await db("usuarios").insert(dados);
 
-                    for(let filtro of dados.perfis) {
-                        const perfil = await obterPerfil(_, {
-                            filtro
-                        })
-                        
-                        if(perfil) {
-                            await db('usuarios_perfis')
-                                .insert({
-                                    perfil_id: perfil.id,
-                                    usuario_id: id
-                                })
-                        }
-                    }
-                }
+      for (let perfil_id of idsPerfis) {
+        await db("usuarios_perfis").insert({ perfil_id, usuario_id: id });
+      }
 
-                delete dados.perfis
-                await db('usuarios')
-                    .where({ id })
-                    .update(dados)
-            }
-            return !usuario ? null : { ...usuario, ...dados }
-        } catch(e) {
-            throw new Error(e)
-        }
+      return db("usuarios").where({ id }).first();
+    } catch (e) {
+      throw new Error(e.sqlMessage);
     }
-}
+  },
+  async excluirUsuario(_, args) {
+    try {
+      const usuario = await obterUsuario(_, args);
+      if (usuario) {
+        const { id } = usuario;
+        await db("usuarios_perfis").where({ usuario_id: id }).delete();
+        await db("usuarios").where({ id }).delete();
+      }
+      return usuario;
+    } catch (e) {
+      throw new Error(e.sqlMessage);
+    }
+  },
+  async alterarUsuario(_, { filtro, dados }) {
+    try {
+      const usuario = await obterUsuario(_, { filtro });
+      if (usuario) {
+        const { id } = usuario;
+        if (dados.perfis) {
+          await db("usuarios_perfis").where({ usuario_id: id }).delete();
+
+          for (let filtro of dados.perfis) {
+            const perfil = await obterPerfil(_, {
+              filtro,
+            });
+
+            if (perfil) {
+              await db("usuarios_perfis").insert({
+                perfil_id: perfil.id,
+                usuario_id: id,
+              });
+            }
+          }
+        }
+
+        if (dados.senha) {
+          // criptografar a senha do usuario
+          const salt = bcrypt.genSaltSync();
+          dados.senha = bcrypt.hashSync(dados.senha, salt);
+        }
+
+        delete dados.perfis;
+        await db("usuarios").where({ id }).update(dados);
+      }
+      return !usuario ? null : { ...usuario, ...dados };
+    } catch (e) {
+      throw new Error(e);
+    }
+  },
+};
+
+module.exports = mutations;
