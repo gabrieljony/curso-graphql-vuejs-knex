3c56001e66227031896a776aa9ba5716e93b4373
Gabriel Jony <gabjony@gmail.com>
2020-05-21 07:50:34 -0300

    Seção 5: Projeto: Backend com GraphQL - 81. Usando Context #02

--- projeto-final/backend/config/context.js
+++ projeto-final/backend/config/context.js
@@ -1,6 +1,46 @@
-module.export = async ({ req }) => {
+const jwt = require("jwt-simple");
+
+module.exports = async ({ req }) => {
   // Apenas em desenvolvimento
-  await require('./simularUsuarioLogado')(req)
+  await require("./simularUsuarioLogado")(req);
   const auth = req.headers.authorization;
   // console.log(auth)
+  const token = auth && auth.substring(7);
+
+  let usuario = null;
+  
+
+  // verificar se o token está valido
+  if (token) {
+    try {
+      let conteudoToken = jwt.decode(token, process.env.APP_AUTH_SECRET);
+
+      if (new Date(conteudoToken.exp * 1000) > new Date()) {
+        usuario = conteudoToken;
+      }
+    } catch (e) {
+      //token inválido
+      throw new Error('Token inválido!')
+    }
+  }
+
+  let admin = false;
+
+  if (usuario && usuario.perfis) {
+    admin = usuario.perfis.includes("admin");
+  }
+
+  const err = new Error('Acesso negado!')
+
+  return {
+    usuario,
+    admin,
+    validarUsuario() {
+        if(!usuario) throw err
+    },
+    validarAdmin() {
+        if(!admin) throw err
+    },
+    
+}
 };
