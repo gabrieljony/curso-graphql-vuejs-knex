44684d9d70e499a963c8a817293ce55a0987da16
Gabriel Jony <gabjony@gmail.com>
2020-05-21 11:39:33 -0300

    Configuração do vue-apollo na aplicação

--- projeto-final/frontend/src/main.js
+++ projeto-final/frontend/src/main.js
@@ -4,10 +4,12 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "@/plugins/vuetify"; // path to vuetify export
+import { apolloProvider } from "./plugins/apollo";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  apolloProvider,
   vuetify,
   router,
   store,
---
+++ projeto-final/frontend/src/plugins/apollo.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+Vue.use(VueApollo)
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:4000',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+export const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
