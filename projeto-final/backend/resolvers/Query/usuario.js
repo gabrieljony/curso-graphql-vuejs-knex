077bc228f095ff11637888791583aac90d420bfb
Gabriel Jony <gabjony@gmail.com>
2020-05-21 00:56:40 -0300

    Seção 5: Projeto: Backend com GraphQL - 78. Login de Usuário

--- projeto-final/backend/resolvers/Query/usuario.js
+++ projeto-final/backend/resolvers/Query/usuario.js
@@ -1,22 +1,35 @@
-const db = require('../../config/db')
+const db = require("../../config/db");
+const bcrypt = require("bcrypt-nodejs");
+const { getUsuarioLogado } = require("../comum/usuario");
 
 module.exports = {
-    usuarios() {
-        return db('usuarios')
-    },
-    usuario(_, { filtro }) {
-        if(!filtro) return null
-        const { id, email } = filtro
-        if(id) {
-            return db('usuarios')
-                .where({ id })
-                .first()
-        } else if(email) {
-            return db('usuarios')
-                .where({ email })
-                .first()
-        } else {
-            return null
-        }
-    },
-}
+  async login(_, { dados }) {
+    const usuario = await db("usuarios").where({ email: dados.email }).first();
+
+    if (!usuario) {
+      throw new Error("Usuário ou Senha inválido.");
+    }
+
+    const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha);
+
+    if (!saoIguais) {
+      throw new Error("Usuário ou Senha inválido.");
+    }
+
+    return getUsuarioLogado(usuario);
+  },
+  usuarios() {
+    return db("usuarios");
+  },
+  usuario(_, { filtro }) {
+    if (!filtro) return null;
+    const { id, email } = filtro;
+    if (id) {
+      return db("usuarios").where({ id }).first();
+    } else if (email) {
+      return db("usuarios").where({ email }).first();
+    } else {
+      return null;
+    }
+  },
+};
